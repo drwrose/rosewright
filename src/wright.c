2959c4e7e0993292eac7fbf67c4e840a5ae3687a
David Rose <david@ddrose.com>
2013-06-24 16:07:41 -0700

    version 1.2

--- src/wright.c
+++ src/wright.c
@@ -9,7 +9,7 @@
 
 PBL_APP_INFO(MY_UUID,
              WATCH_NAME, "drwrose",
-             1, 1, /* App version */
+             1, 2, /* App version */
              RESOURCE_ID_ROSE_ICON,
 #ifdef MAKE_CHRONOGRAPH
              APP_INFO_STANDARD_APP
