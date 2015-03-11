e7392437423b56b41cdab774845ab2e81c36bf82
David Rose <drokgithub@ddrose.com>
2015-03-11 08:49:02 -0700

    d_face

--- resources/clock_faces/make_d_face.py
+++ resources/clock_faces/make_d_face.py
@@ -4,34 +4,39 @@
 
 import FaceMaker
 import PIL.Image
+import sys
 
 face = FaceMaker.FaceMaker(zoom = 0.92)
+#face.drawTicks(60, 0.2, 1.7, width = 0.003); face.save('d_face_ticks.png'); sys.exit()
 
 # 1.0 = 634 ref pixels
 
-face.setFg(255)
-face.fillCircle(1.0)
+#face.setFg(255)
+#face.fillCircle(1.0)
 
-face.setFg(255)
-face.drawCircularSpots(12, 0.968, spotDiameter = 0.05, width = None)
+## face.setFg(255)
+## face.drawCircularSpots(12, 0.968, spotDiameter = 0.05, width = None)
 
 face.setFg(0)
 face.drawRing(1.6, width = 0.25)
+face.drawRing(1.0, width = None)
 
-face.flush()
-ring = PIL.Image.open('d_face_ring.png')
-color, mask = ring.split()
-face.target.paste(color, (0, 0), mask)
-
-face.drawRing(1.0, width = 0.01)
-face.setFg(0)
-face.drawTicks(60, 0.876, 1.0, width = 0.003)
-#face.drawTicks(60, 0.2, 1.7, width = 0.003); face.drawTicks(12, 0.2, 1.7, width = 0.009)
-#face.drawTicks(300, 0.957, 1.0, width = 0.003)
+#face.drawTicks(60, 0.876, 1.0, width = 0.003)
+#face.drawTicks(60, 0.2, 1.7, width = 0.003)
 face.drawCircularSpots(12, 0.95, spotDiameter = 0.05, width = 0.005)
-face.drawRing(0.084, width = 0.0347)
+face.save('d_face_fg.png')
 
+face = FaceMaker.FaceMaker(zoom = 0.92)
+face.setFg(0)
+face.drawRing(1.0, width = 0.01)
+face.drawCircularSpots(12, 0.95, spotDiameter = 0.05, width = None)
+face.save('d_face_bg.png')
+
+face = FaceMaker.FaceMaker(zoom = 0.92)
+face.setFg(0)
+
+face.drawRing(0.084, width = 0.0347)
 font = face.loadFont('Multicolore.otf', 0.13)
 face.drawCircularLabels(face.standardLabels, 0.86, font, align = 'i')
 
-face.save('d_face_working.png')
+face.save('d_face_labels.png')
