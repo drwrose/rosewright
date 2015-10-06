ad641b8841259faaf8caec9c641059b5fa20645c
David Rose <david@ddrose.com>
2015-10-06 10:55:59 -0700

    build accented glyphs

--- config_watch.py
+++ config_watch.py
@@ -282,7 +282,7 @@ faces = {
         'battery_round' : [ (110, 57, 'b'), (132, 33, 'b'),
                             (110, 57, 'b'), (132, 33, 'b'),
                             ],
-        'defaults' : [ 'day:b', 'date:c', 'second', 'hour_minute_overlap', 'pebble_label' ],
+        'defaults' : [ 'day:b', 'date:c', 'second', 'hour_minute_overlap', 'pebble_label', 'limit_cache_aplite' ],
         },
     'c' : {
         'filename' : ['c_face.png'],
