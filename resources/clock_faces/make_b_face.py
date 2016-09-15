a33ab9e54091ef6d344ac7d2f70714b0582d32fb
David Rose <david@ddrose.com>
2016-09-14 18:07:36 -0700

    moving towards emery

--- resources/clock_faces/make_b_face.py
+++ resources/clock_faces/make_b_face.py
@@ -4,11 +4,24 @@
 
 import FaceMaker
 
-# aplite, basalt
-#face = FaceMaker.FaceMaker(zoom = 1.0, screenSize = (144, 168))
+#platform = 'aplite'
+#platform = 'basalt'
+#platform = 'chalk'
+platform = 'emery'
+#layer = 'ticks'
+#layer = 'axle'
+layer = 'numerals'
 
-# chalk
-face = FaceMaker.FaceMaker(zoom = 0.98, screenSize = (180, 180))
+if platform in ['aplite', 'basalt']:
+    face = FaceMaker.FaceMaker(zoom = 1.0, screenSize = (144, 168))
+elif platform == 'chalk':
+    face = FaceMaker.FaceMaker(zoom = 0.98, screenSize = (180, 180))
+elif platform == 'emery':
+    face = FaceMaker.FaceMaker(zoom = 1.0, screenSize = (200, 228))
+
+if layer == 'numerals':
+    font = face.loadFont('Barkentina 1.otf', 0.15)
+    face.drawCircularLabels(face.standardLabels, 0.92, font, align = 'i')
 
 #face.setFg(128)
 #face.drawRing(1.1, width = 0.05)
@@ -16,12 +29,15 @@ face = FaceMaker.FaceMaker(zoom = 0.98, screenSize = (180, 180))
 #face.drawRing(1.1)
 #face.drawRing(1.0)
 
-face.drawRing(0.093, width = 0.028)
+if layer == 'axle':
+    face.drawRing(0.093, width = 0.028)
 
-face.drawTicks(60, 0.95, 1.0, width = 0.003)
-face.drawTicks(12, 0.95, 1.0, width = 0.009)
-
-font = face.loadFont('Barkentina 1.otf', 0.15)
-#face.drawCircularLabels(face.standardLabels, 0.92, font, align = 'i')
+if layer == 'ticks':
+    if platform == 'chalk':
+        face.drawTicks(60, 0.95, 1.0, width = 0.003)
+        face.drawTicks(12, 0.95, 1.0, width = 0.009)
+    else:
+        face.drawTicks(60, 0.2, 1.5, width = 0.003)
+        face.drawTicks(12, 0.2, 1.5, width = 0.009)
 
 face.save('b_face_working.png')
--- resources/clock_faces/make_e_face.py
+++ resources/clock_faces/make_e_face.py
@@ -1,20 +1,24 @@
-# This is a tool to generate a_face.png.  If you already have this
+# This is a tool to generate e_face.png.  If you already have this
 # file and are happy with it, there's no reason to run this tool
 # again.
 
 import FaceMaker
 import PIL.Image
 
-# aplite, basalt
-#face = FaceMaker.FaceMaker(zoom = 1.0, screenSize = (144, 168))
+#platform = 'aplite'
+#platform = 'basalt'
+#platform = 'chalk'
+platform = 'emery'
+layer = 'ticks'
 
-# chalk
-face = FaceMaker.FaceMaker(zoom = 1.0, screenSize = (180, 180))
+if platform in ['aplite', 'basalt']:
+    face = FaceMaker.FaceMaker(zoom = 1.0, screenSize = (144, 168))
+elif platform == 'chalk':
+    face = FaceMaker.FaceMaker(zoom = 1.0, screenSize = (180, 180))
+elif platform == 'emery':
+    face = FaceMaker.FaceMaker(zoom = 1.0, screenSize = (200, 228))
 
-#face.drawTicks(60, 0.5, 2.0, width = 0.003)
-#face.drawTicks(12, 0.5, 2.0, width = 0.01)
-face.drawTicks(60, 0.2, 1.7, width = 0.003); face.drawTicks(12, 0.2, 1.7, width = 0.009)
+if layer == 'ticks':
+    face.drawTicks(60, 0.2, 1.7, width = 0.003); face.drawTicks(12, 0.2, 1.7, width = 0.009)
 
-# 1.0 = 317 ref pixels
-
-face.save('e_face_ticks.png')
+face.save('e_face_working.png')
