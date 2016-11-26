7097d68104cb100e67e58a2ed2c3e9f647524bdf
David Rose <david@ddrose.com>
2016-11-26 11:25:50 -0800

    bump to version 4.20

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 # The current version of the app itself
-version = '4.19'
+version = '4.20'
 
 # The version number of the config pages loaded.  Usually tracks
 # version, above, but may lag by a few versions until it needs to be
